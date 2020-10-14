dca819b8e1339805b34b54b918c79b47bab5a31e
Max <mx181199@gmail.com>
2020-10-14 14:24:14 +0300

    Drop autorization

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require('express')
 const http = require('http');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const authFilter = require('./middlewares/auth-filter');
 const proxy = require('express-http-proxy')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = express()
 const port = 30002;
-
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-if(process.env.NODE_ENV == 'production') {
-  authFilter(app);
-}
 
 app.use('/static', express.static('.'));
 
@@ -24,8 +14,6 @@ app.use('/:host', (req,res,next)=>{
   delete req.params.host;
   proxy(`http://${host}`)(req,res,next);
 })
-
-
 
 const srvr = http.createServer(app);
 srvr.listen(port, () => {
